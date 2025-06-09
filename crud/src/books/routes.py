4100ec23c234e20cad4fccc9f794d1165b8699fc
Kovács Péter <smitpeter777@gmail.com>
2025-06-09 13:51:20 +0200

    add sqlmodel

--- crud/src/books/routes.py
+++ crud/src/books/routes.py
@@ -1,47 +1,43 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 from typing import List
+from crud.src.books.schemas import BookUpdateModel, BookCreateModel
+from crud.src.db.main import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
+from crud.src.books.services import BookService, Book
 
-from crud.src.books.schemas import Book, BookUpdateModel
-from crud.src.books.book_data import books
 
 book_router = APIRouter()
+book_service = BookService()
 
 @book_router.get("/", response_model=List[Book])
-async def get_all_books():
-    return books
+async def get_all_books(session:AsyncSession = Depends(get_session)):
+    books = book_service.get_all_books(session)
+    return await books
 
-@book_router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_book(book_data: Book) -> dict:
-    new_book = book_data.model_dump()
-    books.append(new_book)
-    return new_book
 
-@book_router.get("/{book_id}")
-async def get_book(book_id: int) -> dict:
-    for book in books:
-        if book["id"] == book_id:
-            return book
+@book_router.post("/", status_code=status.HTTP_201_CREATED, response_model=Book)
+async def create_book(book_data:BookCreateModel, session:AsyncSession = Depends(get_session)) -> dict:
+    new_book = book_service.create_book(book_data, session)
+    return await new_book
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+@book_router.get("/{book_uid}", response_model=Book)
+async def get_book(book_uid: str, session:AsyncSession = Depends(get_session)) -> dict:
+    book = await book_service.get_book(book_uid, session)
+    if book is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+    return book
 
-@book_router.patch("/{book_id}")
-async def update_book(book_id: int, book_data: BookUpdateModel) -> dict:
-    for book in books:
-        if book["id"] == book_id:
-            book["title"] = book_data.title
-            book["author"] = book_data.author
-            book["publisher"] = book_data.publisher
-            book["page_count"] = book_data.page_count
-            book["language"] = book_data.language
-            return book
+@book_router.patch("/{book_uid}", response_model=Book)
+async def update_book(book_uid: str, book_data: BookUpdateModel, session:AsyncSession = Depends(get_session)) -> dict:
+    updated_book = await book_service.update_book(book_uid, book_data, session)
+    if updated_book is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+    return  updated_book
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
-
-@book_router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_id: int):
-    for book in books:
-        if book["id"] == book_id:
-            books.remove(book)
-            return {"message": "Book deleted successfully"}
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+@book_router.delete("/{book_uid}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_uid: str, session:AsyncSession = Depends(get_session)):
+    deleted_book = await book_service.delete_book(book_uid, session)
+    if deleted_book is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+    return
 
--- crud/src/db/main.py
+++ crud/src/db/main.py
@@ -1,8 +1,8 @@
 from sqlmodel import SQLModel
 from sqlalchemy.ext.asyncio import create_async_engine
 from crud.src.config import Config
-
-
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 engine = create_async_engine(
     url=Config.DATABASE_URL,
@@ -15,3 +15,12 @@ async def init_db():
 
         await conn.run_sync(SQLModel.metadata.create_all)
 
+async def get_session() -> AsyncSession:
+    Session = sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+
+    async with Session() as session:
+        yield session
